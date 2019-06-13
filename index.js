959a081f79f3c2c6d264c57b2e71899696859009
Daniel Schneider <daschne8@gmail.com>
2019-06-13 01:18:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function logDriversByHometown(drivers,hometown){
   logDriverNames(drivers.filter(function(driver){return driver.hometown === hometown}))
 }
 function driversByRevenue(drivers){
-  newDrivers = [...drivers]
+  let newDrivers = [...drivers]
   newDrivers.sort(function(driver1,driver2){return driver1.revenue - driver2.revenue})
 }
